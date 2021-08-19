77c89395c775b82b914b41861cd3003ddf8a56bc
wangjinsheng <wangjinsheng593@163.com>
2021-08-19 13:40:45 +0800

    fix：项目整理

--- src/main.js
+++ src/main.js
@@ -1,6 +1,28 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+// set ElementUI lang to EN
+// Vue.use(ElementUI, { locale })
+// 如果想要中文版 element-ui，按如下方式声明
+Vue.use(ElementUI)
+
+import Pagination from '@/components/Pagination/index'
+import CQueryForm from '@/components/CQueryForm/index'
+import CTable from '@/components/CTable/index'
+Vue.component('Pagination', Pagination)
+Vue.component('CQueryForm', CQueryForm)
+Vue.component('CTable', CTable)
+
+const context = new Vue({
+    el: '#app',
+    router,
+    store,
+    render: h => h(App)
+})
+window.vm = context
+export default context
